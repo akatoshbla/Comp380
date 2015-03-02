1bbc44a5f3b0db9d41255e63858b4e440708496a
klinster <contact@klinster.com>
2015-03-01 17:21:07 -0800

    More addtions from @screamingeagle39 and a working password validation

--- app/src/main/java/com/comp380/csun/comp380/AddExpenseActivity.java
+++ app/src/main/java/com/comp380/csun/comp380/AddExpenseActivity.java
@@ -1,129 +1,193 @@
 package com.comp380.csun.comp380;
 
+import android.content.Context;
 import android.content.Intent;
-import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListView;
-import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
 
 /**
  * Created by gdfairclough on 2/15/15.
  */
 
-    public class AddExpenseActivity extends ActionBarActivity {
+public class AddExpenseActivity extends ActionBarActivity {
 
-        AutoCompleteTextView categoryBox;
-        EditText vendorBox;
-        EditText costBox;
-        EditText dateBox;
-
-        @Override
-        protected void onCreate(Bundle savedInstanceState){
-            super.onCreate(savedInstanceState);
-
-            //delete database when needed
-            //this.deleteDatabase("expenseTracker.db");
-
-            DatabaseHandler dbHandler = new DatabaseHandler(this, null, null,1);
-
-            //manually insert hardcoded categories for testing purposees
-            //dbHandler.textCategoryValues();
-
-            setContentView(R.layout.activity_main);
-
-            categoryBox = (AutoCompleteTextView) findViewById(R.id.category_input);
-            vendorBox = (EditText) findViewById(R.id.vendor_input);
-            costBox = (EditText) findViewById(R.id.amount_input);
-            dateBox = (EditText) findViewById(R.id.date_input);
-            populateViewer();
+    private AutoCompleteTextView categoryBox;
+    private EditText vendorBox;
+    private EditText costBox;
+    private EditText dateBox;
+    private DatabaseHandler dbHandler;
+    private Button buttonSubmit;
 
 
-            //populate autocompletetextview
-            ArrayAdapter<String> adapter =
-                    new ArrayAdapter<String>(this,R.layout.autocomplete_dropdown_item,
-                            dbHandler.getCategoriesStrings());
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
 
-            categoryBox.setAdapter(adapter);
 
-        }
 
-        public void newExpense (View view){
-            DatabaseHandler dbHandler = new DatabaseHandler(this, null, null,1);
+        dbHandler = new DatabaseHandler(this, null, null,1);
 
-            String category = categoryBox.getText().toString();
-            String vendor = vendorBox.getText().toString();
-            float cost = Float.parseFloat(costBox.getText().toString());
-            String date  = dateBox.getText().toString();
 
-            Expense expense = new Expense(category,vendor,cost,date);
+
+        //manually insert hardcoded categories for testing purposees
+        //dbHandler.testCategoryValues();
+        //dbHandler.testExpenseValues();
+        //dbHandler.deleteCategory(4);
+
+
+        setContentView(R.layout.activity_addexpense);
+
+        buttonSubmit = (Button) findViewById(R.id.submit_button);
+        categoryBox = (AutoCompleteTextView) findViewById(R.id.category_input);
+        vendorBox = (EditText) findViewById(R.id.vendor_input);
+        costBox = (EditText) findViewById(R.id.amount_input);
+        dateBox = (EditText) findViewById(R.id.date_input);
+
+        //disable the button if the text has not been changed
+        buttonSubmit.setEnabled(false);
+        buttonSubmit.setBackgroundColor(getResources().getColor(android.R.color.darker_gray));
+
+        //add text changed listener to the cost field;
+        costBox.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                //enable the button when the text has been changed
+                buttonSubmit.setBackgroundColor(getResources().getColor
+                        (R.color.buttons));
+                buttonSubmit.setEnabled(true);
+                if(costBox.getText().toString().equals("")){
+                    buttonSubmit.setEnabled(false);
+                    buttonSubmit.setBackgroundColor(getResources().getColor(android.R.color.darker_gray));
+                }
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+
+
+        //populate autocompletetextview
+        ArrayAdapter<String> adapter =
+                new ArrayAdapter<String>(this,R.layout.autocomplete_dropdown_item,
+                        dbHandler.getCategoriesStrings());
+
+        categoryBox.setAdapter(adapter);
+
+    }
+
+    public void newExpense (View view){
+        Expense expense;
+        String category;
+        String vendor;
+        Float cost;
+
+        //prepare for toast
+        Context context = getApplicationContext();
+        CharSequence text;
+        int duration = Toast.LENGTH_SHORT;
+
+        //check that necessary fields have been filled out
+        if(costBox.getText().toString().equals("")){
+            //cost is not filled out, so don't add to database
+
+            //TODO needs some serious field validation
+            text = "Please enter a cost";
+            Toast toast = Toast.makeText(context,text,duration);
+            toast.setMargin(5,5);
+            toast.show();
+        }else{
+            //check if category field is blank
+            if(categoryBox.getText().toString().equals("")){
+                category = "Uncategorized";
+            }else{
+                category = categoryBox.getText().toString();
+            }
+            //check if vendor field is blank
+            if(vendorBox.getText().toString().equals("")){
+                vendor = "Unspecified";
+            }else{
+                vendor = vendorBox.getText().toString();
+            }
+
+            //set cost equal to the current value in the editText field
+            cost = Float.parseFloat(costBox.getText().toString());
+
+            //set default date to today if nothing is in the editText field
+            if (!dateBox.getText().toString().equals("")){
+                String date  = dateBox.getText().toString();
+                expense = new Expense(category,vendor,cost,date);
+            }else{
+                expense = new Expense(category,vendor,cost);
+            }
+
 
             dbHandler.addExpense(expense);
             categoryBox.setText("");
             vendorBox.setText("");
             costBox.setText("");
             dateBox.setText("");
-            populateViewer();
-        }
-
-        public void onButtonClick(View view){
-
-            int id = view.getId();
-
-            if(id == R.id.reset_button){
-                categoryBox.setText(null);
-                vendorBox.setText(null);
-                dateBox.setText(null);
-                costBox.setText(null);
-            }
-            else if(id == R.id.submit_button){
-                Intent display = new Intent(this, DataDisplayActivity.class);
-                //add to the database
-                display.putExtra("Cost", costBox.getText().toString());
-                display.putExtra("Category", categoryBox.getText().toString());
-                display.putExtra("Vendor", vendorBox.getText().toString());
-                display.putExtra("Date", dateBox.getText().toString());
-                newExpense(view);
-                startActivity(display);
-            }
-        }
-
-        public void lookupExpense(View view){
-            DatabaseHandler dbHandler = new DatabaseHandler(this,null,null,1);
-
-            Expense expense = dbHandler.findExpense(categoryBox.getText().toString());
-
-            if (expense != null){
-                categoryBox.setText(String.valueOf(expense.getCategory()));
-                vendorBox.setText(String.valueOf(expense.getVendor()));
-                costBox.setText(String.valueOf(expense.getCost()));
-                dateBox.setText(String.valueOf(expense.getDate()));
-            }else{
-                categoryBox.setText("No match found");
-            }
 
         }
 
-        public void removeExpense(View view){
-            DatabaseHandler dbHandler = new DatabaseHandler(this,null,null,1);
 
-            //delete based on primary key from record display screen
+    }
 
+    public void onButtonClick(View view){
+
+        int id = view.getId();
+
+        if(id == R.id.reset_button){
+            categoryBox.setText(null);
+            vendorBox.setText(null);
+            dateBox.setText(null);
+            costBox.setText(null);
         }
+        else if(id == R.id.submit_button){
 
-        public void populateViewer(){
-            DatabaseHandler dbHandler = new DatabaseHandler(this,null,null,1);
-            Cursor cursor = dbHandler.getAllRows();
-            String[] columns = dbHandler.tableNames();
-            int[] ids = new int[] {R.id.checkboxes, R.id.categoryViewer, R.id.venderViewer, R.id.costViewer, R.id.dateViewer};
-            SimpleCursorAdapter myCursorAdapter;
-            myCursorAdapter = new SimpleCursorAdapter(getBaseContext(), R.layout.itemized_layout, cursor, columns, ids,0);
-            ListView myList = (ListView) findViewById(R.id.listViewTasks);
-            myList.setAdapter(myCursorAdapter);
+            //create intent for ExpenseDisplayActivity
+            Intent displayExpenses = new Intent(this, ExpenseDisplayActivity.class);
+
+            //add to the database
+            newExpense(view);
+
+            startActivity(displayExpenses);
         }
     }
 
+    public void lookupExpense(View view){
+
+        Expense expense = dbHandler.findExpense(categoryBox.getText().toString());
+
+        if (expense != null){
+            categoryBox.setText(String.valueOf(expense.getCategory()));
+            vendorBox.setText(String.valueOf(expense.getVendor()));
+            costBox.setText(String.valueOf(expense.getCost()));
+            dateBox.setText(String.valueOf(expense.getDate()));
+        }else{
+            categoryBox.setText("No match found");
+        }
+
+    }
+
+
+
+
+}
--- app/src/main/java/com/comp380/csun/comp380/LoginActivity.java
+++ app/src/main/java/com/comp380/csun/comp380/LoginActivity.java
@@ -2,8 +2,12 @@ package com.comp380.csun.comp380;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.v7.app.ActionBarActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -18,27 +22,68 @@ import java.security.NoSuchAlgorithmException;
 // This class is for if the user has a password in DB
 public class LoginActivity extends ActionBarActivity {
 
-    DatabaseHandler db;
+    private Button mSubmit;
+    private EditText mPassword;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // set the layout, button and text field
         setContentView(R.layout.activity_login);
+        mPassword = (EditText)findViewById(R.id.textPassword);
+        mSubmit = (Button)findViewById(R.id.buttonPassword);
+
+        // set the submit button to be off initially.
+        mSubmit.setEnabled(false);
+        mSubmit.setBackgroundColor(getResources().getColor(android.R.color.darker_gray));
+
+        //region register a text listener
+        mPassword.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mSubmit.setEnabled(true);
+                mSubmit.setBackgroundColor(getResources().getColor(R.color.buttons));
+
+                if (mPassword.getText().toString().equals("")) {
+                    mSubmit.setEnabled(false);
+                    mSubmit.setBackgroundColor(getResources().getColor(android.R.color.darker_gray));
+                }
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+        //endregion
     }
 
+    //region Check the database for the password.
+    /**
+     * Handles the button press.
+     * Pre-Condition: A valid clicked view must be
+     * sent. Assumes the password is not empty.
+     * Post-Condition: Starts the AddExpenseActivity
+     * @param view The view that was clicked.
+     */
     public void onLogin(View view) {
-        db = new DatabaseHandler(this,null,null,1);
-        EditText pw = (EditText)findViewById(R.id.textPassword);
-        if (db.checkPassword(md5(pw.getText().toString()))) {
+        DatabaseHandler db = new DatabaseHandler(this,null,null,1);
+        if (db.checkPassword(md5(mPassword.getText().toString()))) {
             finish();
             startActivity(new Intent(LoginActivity.this, AddExpenseActivity.class));
         }
         else {
-            pw.setText("");
-            Toast.makeText(getApplicationContext(), "Password Mismatch!", Toast.LENGTH_SHORT).show();
+            mPassword.setText(null);
+            mPassword.setError("Invalid!");
         }
     }
+    //endregion
 
-    //MD5 Hash Functions Section
+    //region MD5 Hash Functions Section
     private char[] hextable = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
     private String byteArrayToHex(byte[] array) {
         String s = "";
@@ -70,4 +115,5 @@ public class LoginActivity extends ActionBarActivity {
     private String md5(String s) {
         return digest(s, "MD5");
     }
+    //endregion
 }
